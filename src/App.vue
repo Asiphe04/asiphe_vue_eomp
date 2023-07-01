d195841e73276e166037b3b0b7859d2ab93ac62a
Asiphe04 <asiphendimlana40@gmail.com>
2023-07-01 11:53:05 +0200

    ...

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/projects">Projects</router-link> |
+    <router-link to="/testimonial">Testimonial</router-link> |
+    <router-link to="/contact">Contact</router-link> |
   </nav>
   <router-view/>
 </template>
