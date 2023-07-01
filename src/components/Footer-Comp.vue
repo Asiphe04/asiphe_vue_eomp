9f591c36bbd3389959babf0aaae476d82cc62d39
Asiphe04 <asiphendimlana40@gmail.com>
2023-07-01 13:51:52 +0200

    footer_done

---
+++ src/components/Footer-Comp.vue
@@ -0,0 +1,69 @@
+<template>
+    <section class="text-light py-3">
+      <hr class="my-4" />
+
+      <div class="footer-container text-center">
+        <p class="text-white">
+          Copyright &copy; 2023 All Rights Reserved by Asiphe Ndimlana
+        </p>
+        <div class="social-icons">
+          <a
+            href="https://www.facebook.com/profile.php?id=100089087082905"
+            target="_blank"
+            ><i class="fa-brands fa-facebook" id="Facebook"></i
+          ></a>
+          <a href="https://twitter.com/ndim_asiphe" target="_blank"
+            ><i class="fa-brands fa-twitter" id="Twitter"></i
+          ></a>
+          <a href="https://www.linkedin.com/public-profile/settings?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_self_edit_contact-info%3B8h9wTBRuSt2U75xVavKEoA%3D%3D" target="_blank"
+            ><i class="fa-brands fa-linkedIn" id="linkedIn"></i
+          ></a>
+          <a href="https://github.com/Asiphe04" target="_blank"
+            ><i class="fa-brands fa-github" id="Github"></i
+          ></a>
+        </div>
+      </div>
+    </section>
+</template>
+<script>
+    export default{}
+</script>
+<style>
+
+section {
+  background-color: #010409 !important;
+}
+#Facebook:hover {
+  color: rgb(2, 8, 120) !important;
+}
+#Twitter:hover {
+  color: skyblue !important;
+}
+#Instagram:hover {
+  color: purple !important;
+}
+#Github:hover {
+  color: green !important;
+}
+#Youtube:hover {
+  color: red !important;
+}
+#linkedIn:hover {
+  color: blue !important;
+}
+.footer-container a {
+  color: white !important;
+  margin: 5px !important ;
+  padding: 5px !important;
+  font-size: x-large !important;
+}
+.social-icons a {
+  color: white !important;
+  margin: 5px !important ;
+  padding: 5px !important;
+  font-size: x-large !important;
+}
+
+</style>
+
+
