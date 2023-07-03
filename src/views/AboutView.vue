82af3da83311c065f58189807fc129fd7ea3597f
Asiphe04 <asiphendimlana40@gmail.com>
2023-07-03 09:01:43 +0200

    busy_with_about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page, LOSER!!</h1>
+  <h1 class="text-center">About me</h1>
+  <div class="container d-flex m-5">
+      <div class="content col-6 d-flex" id="content-img"></div>
+      <div class="content col-6 d-flex">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt recusandae minus distinctio ipsa eius enim consectetur, nisi reiciendis, maiores pariatur consequuntur, corporis iure minima veritatis quia assumenda culpa maxime eveniet.</p>
+      </div>
+
+   
   </div>
 </template>
+
+<script>
+export default{
+  
+}
+</script>
+<style scoped>
+#content-img{
+  /* border-radius: 6%; */
+    height: 469px;
+  
+  background: no-repeat;
+ background-image: url("https://i.postimg.cc/rwrkhW0f/cook.jpg");
+}
+
+</style>
