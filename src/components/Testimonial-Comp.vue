15cd251920d765138dd5281676f4ceb3454ee830
Asiphe04 <asiphendimlana40@gmail.com>
2023-07-04 20:34:34 +0200

    added scrollable thing

--- src/components/Testimonial-Comp.vue
+++ src/components/Testimonial-Comp.vue
@@ -1,3 +1,10 @@
 <template>
     <h3>This is testimonial Component</h3>
+
+
 </template>
+<script>
+export default {
+    
+}
+</script>
--- src/views/TestimonialView.vue
+++ src/views/TestimonialView.vue
@@ -1,3 +1,37 @@
 <template>
 <h1>This is testimonial page</h1>
+    <div class="scrollable-div">
+  <!-- Content goes here -->
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+</div>
 </template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.scrollable-div {
+    border-radius: 2px solid green;
+  height: 600px; /* Set the desired height */
+  width: 100%; /* Set the desired width */
+  overflow: auto; /* Enable scrolling when content exceeds the div's dimensions */
+}
+
+</style>
