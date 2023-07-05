3b6d3874c4291fd82f2f6ed062c1d37a63a3ebdf
Asiphe04 <asiphendimlana40@gmail.com>
2023-07-05 10:06:53 +0200

    ...

--- src/views/TestimonialView.vue
+++ src/views/TestimonialView.vue
@@ -1,24 +1,19 @@
 <template>
 <h1>This is testimonial page</h1>
-    <div class="scrollable-div">
-  <!-- Content goes here -->
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
-  <h1>LOSer</h1>
+  <div class="container d-flex">
+    <div class="col-8">
+    </div>
+    <div class="col-4 scrollable-div">
+      <h1>Testimonial</h1>
+      <h1>Testimonial</h1>
+      <h1>Testimonial</h1>
+      <h1>Testimonial</h1>
+      <h1>Testimonial</h1>
+      <h1>Testimonial</h1>
+      <h1>Testimonial</h1>
+      <h1>Testimonial</h1>
+      <h1>Testimonial</h1>
+    </div>
 </div>
 </template>
 <script>
@@ -39,9 +34,11 @@
 <style scoped>
 .scrollable-div {
     border-radius: 2px solid green;
-  height: 600px; /* Set the desired height */
-  width: 100%; /* Set the desired width */
-  overflow: auto; /* Enable scrolling when content exceeds the div's dimensions */
+  height: 600px; 
+  width: 100%; 
+  overflow: auto; 
 }
-
+.container{
+  background-image: url("https://i.postimg.cc/Gm7wP3rp/hulu-1.png");
+}
 </style>
