86e6682d3e495b9a8c995ff1d4c0d557125ff243
Asiphe04 <asiphendimlana40@gmail.com>
2023-07-05 10:13:09 +0200

    ..

--- src/views/TestimonialView.vue
+++ src/views/TestimonialView.vue
@@ -1,44 +1,33 @@
 <template>
 <h1>This is testimonial page</h1>
-  <div class="container d-flex">
-    <div class="col-8">
-    </div>
-    <div class="col-4 scrollable-div">
-      <h1>Testimonial</h1>
-      <h1>Testimonial</h1>
-      <h1>Testimonial</h1>
-      <h1>Testimonial</h1>
-      <h1>Testimonial</h1>
-      <h1>Testimonial</h1>
-      <h1>Testimonial</h1>
-      <h1>Testimonial</h1>
-      <h1>Testimonial</h1>
-    </div>
+    <div class="scrollable-div">
+  <!-- Content goes here -->
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
+  <h1>LOSer</h1>
 </div>
 </template>
-<script>
-  import TestimonialCardComp from "@/components/Testimonial-Card-Comp.vue"
-  export default {
-    computed:{
-      testimonials(){
-        return this.$store.state.testimonials
-      }
-    },
-      mounted(){
-        this.$store.dispatch("getTestimonials")
-      },
-      components: {TestimonialCardComp}
-   
-  }
-</script>
+
 <style scoped>
 .scrollable-div {
     border-radius: 2px solid green;
-  height: 600px; 
-  width: 100%; 
-  overflow: auto; 
+  height: 600px; /* Set the desired height */
+  width: 100%; /* Set the desired width */
+  overflow: auto; /* Enable scrolling when content exceeds the div's dimensions */
 }
-.container{
-  background-image: url("https://i.postimg.cc/Gm7wP3rp/hulu-1.png");
-}
+
 </style>
