8627816f6489d8110247e5182d57dbcf3aba539e
Asiphe04 <asiphendimlana40@gmail.com>
2023-07-05 09:47:40 +0200

    added testimonials on store

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,15 @@ export default createStore({
   state: {
     project: null,
     projects: null,
+    testimonial: null,
+    testimonials: null,
   },
   mutations: {
     setProjects: (state, projects) => {
       state.projects = projects;
+    },
+    setTestimonials: (state, testimonials) => {
+      state.testimonials = testimonials;
     },
   },
   actions: {
@@ -15,6 +20,13 @@ export default createStore({
       fetch("https://asiphe04.github.io/data/projects.json")
         .then((response) => response.json())
         .then((projects) => context.commit("setProjects", projects));
+    },
+    getTestimonials: async (context) => {
+      fetch("https://asiphe04.github.io/data/testimonials.json")
+        .then((response) => response.json())
+        .then((testimonials) =>
+          context.commit("getTestimonials", testimonials)
+        );
     },
   },
 });
