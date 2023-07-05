c41d7c0d38df31a8ef1a137c02fed1dca3259e80
Asiphe04 <asiphendimlana40@gmail.com>
2023-07-05 15:58:08 +0200

    first commit

---
+++ src/components/Skills-Comp.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <div :to="{name: 'skills', params: {id: skill.id}}">
+   <div class="img-container">
+
+   </div>
+</div> -->
+
+</template>
+<script>
+    //   export default{
+    //   props: ["skill"]
+    // }
+</script>
+<h1>hi</h1>
+<style scoped>
+</style>
