8783905545eb0be635a9f01fee92d4be5dfb7d75
Asiphe04 <asiphendimlana40@gmail.com>
2023-11-03 11:40:57 +0200

    removed some social icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,14 +12,7 @@
           Copyright &copy; 2023 All Rights Reserved by Asiphe Ndimlana
         </p>
         <div class="social-icons">
-          <a
-            href="https://www.facebook.com/profile.php?id=100089087082905"
-            target="_blank"
-            ><i class="fa-brands fa-facebook" id="Facebook"></i
-          ></a>
-          <a href="https://twitter.com/ndim_asiphe" target="_blank"
-            ><i class="fa-brands fa-twitter" id="Twitter"></i
-          ></a>
+        
           <a href="https://www.linkedin.com/public-profile/settings?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_self_edit_contact-info%3B8h9wTBRuSt2U75xVavKEoA%3D%3D" target="_blank"
             ><i class="fa-brands fa-linkedin" id="linkedIn"></i
           ></a>
